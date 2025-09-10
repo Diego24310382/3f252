7817d83c06916a5abcd84256cab697fce99650bb
Diego24310382 <a24310382@live.ceti.mx>
2025-09-10 02:34:32 +0000

    practica 1

---
+++ include/Foco.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <EstadoFoco.hpp>
+class Foco
+{
+private:
+    EstadoFoco estado;
+
+public:
+    Foco() {}
+    ~Foco() {}
+    void Encender() { estado = true; }
+    void Apagar() { estado = false; }
+    EstadoFoco LeerEstado()
+    {
+        return estado;
+    }
+};
